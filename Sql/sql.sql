2a38d3f55ccb4ae66cbee5d70b11d0a1898c6b65
MS-GSGGJBEZYCCI\Administrator <847212939@qq.com>
2022-05-12 21:17:24 +0800

    数据库

--- Sql/sql.sql
+++ Sql/sql.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 11/05/2022 01:40:06
+ Date: 12/05/2022 21:16:05
 */
 
 SET NAMES utf8mb4;
@@ -22,9 +22,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `useraccount`;
 CREATE TABLE `useraccount`  (
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
-  `passward` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`name`) USING BTREE
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_croatian_ci ROW_FORMAT = Dynamic;
+  `userid` varchar(255) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
+  `data` varchar(1024) CHARACTER SET utf8 COLLATE utf8_croatian_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`userid`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_croatian_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Table structure for userid
+-- ----------------------------
+DROP TABLE IF EXISTS `userid`;
+CREATE TABLE `userid`  (
+  `userid` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `data` varchar(1024) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`userid`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
