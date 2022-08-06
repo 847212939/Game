0c22bad0a7b53e7126d5480f759c9e7439832ca9
MS-GSGGJBEZYCCI\Administrator <847212939@qq.com>
2022-08-06 12:36:02 +0800

    1

--- Player/TimerCmd.h
+++ Player/TimerCmd.h
@@ -3,8 +3,8 @@
 // 定时器消息
 enum class TimerCmd
 {
-	TimerCmd_Begin = 0, // 消息开始
-	TimerCmd_Test = 1, // 测试定时器
+	TimerCmd_Begin                     = 0, // 消息开始
+	TimerCmd_Test                      = 1, // 测试定时器
 
 	TimerCmd_End                            // 消息结束                
 };
