906a46a3eda8cb57ea0a72c0fc13bacfa3dbdf18
MS-GSGGJBEZYCCI\Administrator <847212939@qq.com>
2022-08-11 17:56:27 +0800

    1

--- Logic/Animal.h
+++ Logic/Animal.h
@@ -10,15 +10,15 @@ public:
 	virtual AnimalType GetType() = 0;
 
 public:
-	uint64_t GetID();
-	void SetID(uint64_t id);
 	int GetSceneid();
-	void SetSceneid(int sceneid);
+	uint64_t GetID();
 	const Transform& GetTransform();
+	void SetID(uint64_t id);
+	void SetSceneid(int sceneid);
 	void SetTransform(Transform& transform);
 
 private:
-	uint64_t	m_Id;
 	int			m_Sceneid;
+	uint64_t	m_Id;
 	Transform	m_Transform;
 };
