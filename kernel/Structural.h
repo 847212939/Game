ff5f072b3b74c1cd1b7dbddbcab678653d38170e
MS-GSGGJBEZYCCI\Administrator <847212939@qq.com>
2022-08-15 19:28:11 +0800

    1

--- kernel/Structural.h
+++ kernel/Structural.h
@@ -102,10 +102,10 @@ enum class HeroType
 
 enum class ActType
 {
-	at_section_open = 1,			// 时间区间
-	at_always_open = 2,			// 全天开启
-	at_service_open = 3,			// 开服活动
-	at_timed_open = 4,			// 固定时间开启
+	at_section_open = 1,														// 时间区间
+	at_always_open = 2,															// 全天开启
+	at_service_open = 3,														// 开服活动
+	at_timed_open = 4,															// 固定时间开启
 };
 
 class Animal;
