f3fa15692a402a0320ef3b2a4f67ac1807463c62
MS-GSGGJBEZYCCI\Administrator <847212939@qq.com>
2022-08-18 20:41:47 +0800

    1

--- Logic/ActivityHallSys.h
+++ Logic/ActivityHallSys.h
@@ -1,5 +1,13 @@
 #pragma once
 
+enum class ActType
+{
+	at_section_open		= 1,		// 时间区间
+	at_always_open		= 2,		// 全天开启
+	at_service_open		= 3,		// 开服活动
+	at_timed_open		= 4,		// 固定时间开启
+};
+
 class ActivityHallSys
 {
 public:
