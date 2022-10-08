e56a74116be12b1bf0514fb19a1ce1fbc0e06955
liuzhuang <847212939@qq.com>
2022-10-08 18:25:10 +0800

    1

--- kernel/Netmsg.cpp
+++ kernel/Netmsg.cpp
@@ -33,14 +33,15 @@ void Netmsg::split(std::string& str, std::string separator, size_t count)
 
 	for (size_t i = 0; i < size; i++)
 	{
-		pos = str.find(separator, i);
-		if (pos == std::string::npos)
-		{
-			break;
-		}
+		// 最后一次拆分
 		if (count > 0 && ++cnt >= count)
 		{
 			m_SplitsVec.push_back(str.substr(i, size - i));
+			break;
+		}
+		pos = str.find(separator, i);
+		if (pos == std::string::npos)
+		{
 			break;
 		}
 		m_SplitsVec.push_back(str.substr(i, pos - i));
