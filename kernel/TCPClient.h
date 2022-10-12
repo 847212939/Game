5988109e13035f45858657dfeed42b8b3b5f2745
liuzhuang <847212939@qq.com>
2022-10-12 16:59:49 +0800

    1

--- kernel/TCPClient.h
+++ kernel/TCPClient.h
@@ -12,20 +12,30 @@ protected:
 	TCPClient& operator=(const TCPClient& my);
 
 public:
+	// 初始化
 	bool Init(ServiceType serverType);
+	// 通知所有线程进行一次循环
 	void NotifyAll();
 
 public:
+	// 获取玩家预处理
 	PlayerPrepClient* GetPlayerPrepClient();
 
 private:
+	// 主线程运行都是单线程
+	// 定时器回调函数
 	void TimerCallback(void* pDataLineHead);
+	// 主消息回调函数
 	void SocketCallback(void* pDataLineHead);
+	// 断开链接回调函数
 	void CloseSocketCallback(void* pDataLineHead);
+	// 回调到TimerCallback or SocketCallback or CloseSocketCallback 中
 	bool CallBackFun(SysMsgCmd cmd, void* pDataLineHead);
+	// 添加回调函数
 	void AddNetTypeCallback(SysMsgCmd cmd, std::function<void(void* pDataLineHead)>&& fun);
 
 private:
+	// 接收队列中消息的分发
 	void HandlerRecvDataListThread();
 	void HandleRecvData(ListItemData* pListItem);
 
