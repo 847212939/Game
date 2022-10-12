ac95d13384ef1dcc23f13b54cac969ff6274b8d5
liuzhuang <847212939@qq.com>
2022-10-12 17:08:40 +0800

    1

--- Player/Player.h
+++ Player/Player.h
@@ -21,21 +21,35 @@ public:
 
 public:
 	// 上下线处理
+	// 上线属性计算
 	void CalAttrs();
+	// 上线加载数据库
 	void LoadMysql();
+	// 上线进入场景
 	void EnterScene();
+	// 下线退出游戏
 	void ExitGame(SocketCloseLine* pSocketClose);
 
 public:
+	// 属性回调函数
 	void AttrsCallBackFun();
+	// 数据库回调函数
 	void MysqlCallBackFun();
+	// 进入场景回调函数
 	void EnterSceneCallBackFun();
+	// 退出游戏回调函数
 	void ExitCallBackFun(SocketCloseLine* pSocketClose);
+	// 网络消息回调函数
 	void NetCallBackFun(MsgCmd cmd, PlayerInfo* playerInfo);
+	// 添加属性回调函数
 	void AddAttrsCallback(std::function<void()>&& fun);
+	// 添加进入场景回调函数
 	void AddEnterSceneCallback(std::function<void()>&& fun);
+	// 添加退出游戏回调函数
 	void AddExitCallback(std::function<void(SocketCloseLine*)>&& fun);
+	// 添加网络消息回调函数
 	void AddNetCallback(MsgCmd cmd, std::function<void(PlayerInfo*)>&& fun);
+	// 添加数据库回调函数
 	void AddMysqlCallback(std::string name, std::function<void(std::string&)>&& fun);
 
 public:
