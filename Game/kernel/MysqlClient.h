3a98862a356ca3bca83afc9fa8516d1bd65f2ac8
liuzhuang <847212939@qq.com>
2022-10-24 12:48:08 +0800

    1

--- Game/kernel/MysqlClient.h
+++ Game/kernel/MysqlClient.h
@@ -4,13 +4,13 @@ enum class DataBaseSysMsgCmd
 {
 	cs_load_player			= 1,	// 加载玩家数据库
 	cs_load_global			= 2,	// 加载全局数据库
-	cs_save_replace_player	= 3,	// 保存玩家数据库
-	cs_save_replace_global	= 4,	// 保存全局数据库
-	cs_create_global		= 5,	// 创建全局数据库
-	cs_create_player		= 6,	// 创建玩家数据库
-	cs_load_login			= 7,	// 加载登录数据库
-	cs_create_login			= 8,	// 创建登录数据库
-	cs_save_replace_login	= 9,	// 保存全局数据库
+	cs_load_login			= 3,	// 加载登录数据库
+	cs_save_replace_player	= 4,	// 保存玩家数据库
+	cs_save_replace_global	= 5,	// 保存全局数据库
+	cs_save_replace_login	= 6,	// 保存全局数据库
+	cs_create_global		= 7,	// 创建全局数据库
+	cs_create_player		= 8,	// 创建玩家数据库
+	cs_create_login			= 9,	// 创建登录数据库
 };
 
 class MysqlClient
