35157364c4c50f4456ad2d208865a6411ac190dd
liuzhuang <847212939@qq.com>
2022-11-01 22:09:48 +0800

    1

--- Cxxdll/Cxxdll/DataLine.cpp
+++ Cxxdll/Cxxdll/DataLine.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 
-CDataLine::CDataLine() : m_dataListSize(0)
+CDataLine::CDataLine()
 {
 	m_dataList.clear();
 }
@@ -27,7 +27,7 @@ unsigned int CDataLine::AddData(void* pData, unsigned int uDataSize, SysMsgCmd u
 	{
 		return 0;
 	}
-	if (m_dataListSize > MAX_DATALINE_LEN)
+	if (m_dataList.size() > MAX_DATALINE_LEN)
 	{
 		return 0;
 	}
@@ -45,7 +45,6 @@ unsigned int CDataLine::AddData(void* pData, unsigned int uDataSize, SysMsgCmd u
 
 	m_mutex.lock();
 	m_dataList.push_back(pListItem);
-	m_dataListSize++;
 	m_mutex.unlock();
 
 	m_cond.notify_one();
@@ -58,15 +57,10 @@ unsigned int CDataLine::GetData(ListItemData** pDataBuffer, bool& run, unsigned 
 	*pDataBuffer = nullptr;
 
 	std::unique_lock<std::mutex> uniqLock(m_mutex);
-	m_cond.wait(uniqLock);
-	if (this->GetDataCount() <= 0 || m_dataList.size() <= 0)
-	{
-		uniqLock.unlock();
-		return 0;
-	}
+	m_cond.wait(uniqLock, [this] {return this->m_dataList.size() > 0; });
+
 	*pDataBuffer = m_dataList.front();
 	m_dataList.pop_front();
-	m_dataListSize--;
 	uniqLock.unlock();
 
 	if (!(*pDataBuffer))
@@ -95,13 +89,10 @@ bool CDataLine::CleanData()
 		}
 		SafeDelete(pListItem);
 	}
-
-	m_dataListSize = 0;
-
 	return true;
 }
 
-size_t CDataLine::GetDataCount()
+std::condition_variable& CDataLine::GetConditionVariable()
 {
-	return m_dataListSize;
+	return m_cond;
 }
--- Cxxdll/Cxxdll/DataLine.h
+++ Cxxdll/Cxxdll/DataLine.h
@@ -14,21 +14,13 @@ protected:
 public:
 	// 清理所有数据
 	bool CleanData();
-	// 获取队列大小
-	size_t GetDataCount();
 	// 加入消息队列
 	unsigned int AddData(void* pData, unsigned int uDataSize, SysMsgCmd uDataKind = SysMsgCmd::HD_SYS_MSG_BEGIN);
 	// 提取消息数据
 	unsigned int GetData(ListItemData** pDataBuffer, bool& run, unsigned int& uDataKind);
-
-public:
-	std::condition_variable& GetConditionVariable()
-	{
-		return m_cond;
-	}
+	std::condition_variable& GetConditionVariable();
 
 private:
-	unsigned int				m_dataListSize;
 	std::list <ListItemData*>	m_dataList;
 	std::mutex					m_mutex;
 	std::condition_variable		m_cond;
